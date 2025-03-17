3858cbb19c47a530813074479b2baca59d6c6dfe
Alejandra2207 <malejadr22hotmail.com>
2025-03-17 10:05:16 -0500

    formulario.html, js, css usuarioRol.js, php

---
+++ js/usuarioRol.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    function cargarRoles() {
+        $.ajax({
+            url: '../php/usuarioRol.php',
+            type: 'POST',
+            success: function (data) {
+                let roles = JSON.parse(data);
+                let select = $('#rol');
+                select.empty();
+                select.append('<option value="">Seleccionar</option>');
+                select.append('<option value="1">Instructor</option>');
+                select.append('<option value="2">Visitante</option>');
+                roles.forEach(function (rol) {
+                    select.append(`<option value="${rol.IdRol}">${rol.Rol}</option>`);
+                });
+            }
+        });
+    }
+
+    cargarRoles();
+
+    $('#usuarioForm').submit(function (e) {
+        e.preventDefault();
+        
+        let usuarioData = {
+            documento: $('#Documento').val(),
+            nombre: $('#Nombre').val(),
+            email: $('#Email').val(),
+            celular: $('#Celular').val(),
+            rol: $('#rol').val()
+        };
+
+        $.ajax({
+            url: '../php/usuarioRol.php',
+            type: 'POST',
+            data: usuarioData,
+            success: function (response) {
+                alert(response);
+                $('#usuarioForm')[0].reset();
+            }
+        });
+    });
+});
